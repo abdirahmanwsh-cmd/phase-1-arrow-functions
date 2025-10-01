dec64c40a283f96ca876eeeb8c176f8a5405392e
Manka.code <Abdirahman.wsh@gmail.com>
2025-09-30 23:55:25 -0400

    adds index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+  return 2000 / 100;
+}
+const square = (x) => x * x;
+const add = (a, b) => a + b;
+
+console.log(divide());
+console.log(square(2));
+console.log(add(3, 4));
